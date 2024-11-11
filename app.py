f058f9bc5dad44585c4442f1cd5bb8238aa7acc8
tanush27 <tanush.jadhav.btech2021@sitpune.edu.in>
2024-11-12 00:12:57 +0530

    Made chnages

--- app.py
+++ app.py
@@ -1,15 +1,9 @@
 from flask import Flask, request, jsonify, render_template
 from flask_cors import CORS
-from keras.models import model_from_json
+from tensorflow.keras.models import load_model
 
 app = Flask(__name__, static_url_path='/static')
 CORS(app)
-
-file = open('LSTM Model/model.json', 'r')
-loaded  = file.read()
-file.close()
-
-model = model_from_json(loaded)
 
 @app.route('/')
 def home():
@@ -30,21 +24,21 @@ def predict():
         print(temp1, temp2, temp3, temp4, temp5, temp6)
 
         if sel_model == "gear_oil_temp":
-            model.load_weights("LSTM Model/Gear oil temperature (°C).csv.h5")
+            model = load_model("LSTM Model/Gear oil temperature (°C).csv.h5")
         elif sel_model == "gear_oil_inlet_temp":
-            model.load_weights("LSTM Model/Gear oil inlet temperature (°C).csv.h5")
+            model = load_model("LSTM Model/Gear oil inlet temperature (°C).csv.h5")
         elif sel_model == "front_bearing_temp":
-            model.load_weights("LSTM Model/Front bearing temperature (°C).csv.h5")
+            model = load_model("LSTM Model/Front bearing temperature (°C).csv.h5")
         elif sel_model == "rear_bearing_temp":
-            model.load_weights("LSTM Model/Rear bearing temperature (°C).csv.h5")
+            model = load_model("LSTM Model/Rear bearing temperature (°C).csv.h5")
         elif sel_model == "gen_bearing_front_temp":
-            model.load_weights("LSTM Model/Generator bearing front temperature (°C).csv.h5")
+            model = load_model("LSTM Model/Generator bearing front temperature (°C).csv.h5")
         elif sel_model == "gen_bearing_rear_temp":
-            model.load_weights("LSTM Model/Generator bearing rear temperature (°C).csv.h5")
+            model = load_model("LSTM Model/Generator bearing rear temperature (°C).csv.h5")
         elif sel_model == "rotor_bearing_temp":
-            model.load_weights("LSTM Model/Rotor bearing temp (°C).csv.h5")
+            model = load_model("LSTM Model/Rotor bearing temp (°C).csv.h5")
         else:
-            model.load_weights("LSTM Model/Stator temperature 1 (°C).csv.h5")
+            model = load_model("LSTM Model/Stator temperature 1 (°C).csv.h5")
 
         # prediction = model.predict([[temp1, temp2, temp3, temp4, temp5, temp6]])[0,0]
         prediction = model.predict([[[temp1, temp2, temp3, temp4, temp5, temp6]]])[0,0]
